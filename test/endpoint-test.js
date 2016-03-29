c0983dee402b3d00913ef97e0fefdf2112fd9247
Rory Donohue <rory@kindeo.com>
2016-03-29 11:07:44 +0100

    Adding a few more tests.

--- test/endpoint-test.js
+++ test/endpoint-test.js
@@ -31,4 +31,35 @@ describe('given a server', function() {
                 });
         });
     });
+
+    describe('When /hello/{name} is requested (DELETE)', function() {
+
+        it('responds with 204', function(done){
+            client
+                .del('/hello/george', function(e, req, res) {
+                    should.equal(res.statusCode, 204);
+                    done();
+                });
+        });
+    });
+
+    describe('When /hello is requested (POST)', function() {
+
+        it('responds with 201', function(done){
+            client
+                .post('/hello', function(e, req, res) {
+                    should.equal(res.statusCode, 201);
+                    done();
+                });
+        });
+
+        it('responds with a username of 8 characters', function(done){
+            client
+                .post('/hello', function(e, req, res) {
+                    should(res.body).be.type('string');
+                    should(res.body.length).equal(10);
+                    done();
+                });
+        });
+    });
 });
